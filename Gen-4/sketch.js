b7198a9a625b1bcda34932508cce0403bf5b30d4
ian-gpx-c3der <ian-gpx-c3der@github.com>
2019-04-22 00:42:32 -0300

    +1 artes

---
+++ Gen-4/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  stroke(0,255,255)
+  c = random(25)
+  strokeWeight(4)
+  noFill()
+  k=random()/random()
+}
+
+function draw() {
+      translate(width/2,height/2)
+beginShape()
+  for(i=0;i<TWO_PI*10;i+=0.01){
+    r = 200*cos(k*i)
+    x = r*cos(i)
+    y = r*sin(i)
+    vertex(x,y)
+  }
+  endShape()
+  
+}
